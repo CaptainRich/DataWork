7d9c36d3c963fdfcf812ff6bd724350f9a5da4e8
RichA <rich_ay@sbcglobal.net>
2024-03-27 00:36:50 -0500

    Refactoring, updated plot title with date range for the data.

--- eq_explore.py
+++ eq_explore.py
@@ -4,6 +4,7 @@
 from pathlib import Path      # for directory operations
 import json                   # for JSON file I/O
 import plotly.express as px   # for plotting the data on the world map
+import datetime               # for date labeling of the plot
 
 # Set our 'debug' flag to control status printing.
 debug = True
@@ -17,23 +18,19 @@ all_eq_data = json.loads( contents )                    # convert to a Python ob
 
 # Generate a list of all the earthquakes, and output the length of the list
 all_eq_dicts = all_eq_data[ 'features' ]
+num_quakes = len(all_eq_dicts)
 if debug:
-    print( f"There are {len(all_eq_dicts)} earthquake records in this file." )
+    print( f"There are {num_quakes} earthquake records in this file." )
 
 # Generate a list of all of the earthquake magnitudes, longitudes, and latitudes.  
 # Print the list and its length.
 magnitudes, longitudes, latitudes, eq_titles = [], [], [], []
 
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    tit = eq_dict['properties']['title']
-
-    magnitudes.append( mag )
-    longitudes.append( lon )
-    latitudes.append( lat )
-    eq_titles.append( tit )
+    magnitudes.append( eq_dict['properties']['mag'] )             # magnitude
+    longitudes.append( eq_dict['geometry']['coordinates'][0] )    # longitude
+    latitudes.append( eq_dict['geometry']['coordinates'][1] )     # latitude
+    eq_titles.append( eq_dict['properties']['title'] )            # title (location)
 
 if debug:
     print( f"There are {len(magnitudes)} magnitude records in this file, they are:" )
@@ -48,8 +45,22 @@ if debug:
     readable_contents = json.dumps( all_eq_data, indent=4 ) # create a string with a JSON representation of the data
     path.write_text( readable_contents )
 
+
+# Get the first and last timestamps from the file of earthquake data
+time1 = all_eq_dicts[0]['properties']['time']
+time2 = all_eq_dicts[num_quakes-1]['properties']['time']
+
+date1 = datetime.datetime.fromtimestamp( time1/1e3 )
+date2 = datetime.datetime.fromtimestamp( time2/1e3 )
+
+date_label = str(date2)[:10] + ' to ' + str(date1)[:10]
+
+if debug:
+    print( f"Earthquake 1 time/date: {time1}, {date1}" )
+    print( f"Earthquake 2 time/date: {time2}, {date2}" )
+
 # Setup the plot
-title = 'Global Earthquakes'
+title = 'Global Earthquakes: ' + date_label
 fig = px.scatter_geo( lat=latitudes, lon=longitudes, size=magnitudes, title=title,
                      color=magnitudes,
                      color_continuous_scale='Viridis',
